f097b5fab55f78b3d94197718286a42dea67dbab
Marlon Falcón Hernández <falconsoft.3d@gmail.com>
2022-12-31 15:31:57 +0100

    sc

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
 import '../styles/globals.css'
 import Script from 'next/script'
+import $ from 'jquery';
 
 export default function App({ Component, pageProps }) {
   return (
      <>
-        <Script src="vendor/jquery/jquery.min.js" />
+         <Script src="vendor/jquery/jquery.min.js" />
         <Script src="vendor/jquery-easing/jquery.easing.min.js" />
         <Script src="vendor/bootstrap/js/bootstrap.bundle.min.js" />
         <Script src="js/sb-admin-2.min.js" />
--- pages/api/apus-spain/index.js
+++ pages/api/apus-spain/index.js
@@ -1,30 +1,32 @@
 const target = `https://raw.githubusercontent.com/falconsoft3d/preciosconstruccion/main/public/data/apus/spain/apus.csv`; //file
 
+
 export default async function handler(req, res) {
-    await fetch(target).then(function (response) {
-        let reader = response.body.getReader();
-        let decoder = new TextDecoder('utf-8');
-        console.log("A");
-        return reader.read().then(function (result) {
-             return decoder.decode(result.value);
-         });
-    }).then(function (csv) {
-        console.log("B");
-        let lines = csv.split("\n");
-        let result = [];
-        let headers = lines[0].split(",");
-        let counter = 0;
-        for (let i = 1; i < lines.length; i++) {
-            counter++;
-            let obj = {};
-            let currentline = lines[i].split(",");
-            for (let j = 0; j < headers.length; j++) {
-                obj[headers[j]] = currentline[j];
-            }
-            result.push(obj);
+    try {
+      const response = await fetch(target);
+      const reader = response.body.getReader();
+      const decoder = new TextDecoder('utf-8');
+      console.log("A");
+      let result = await reader.read();
+      const csv = decoder.decode(result.value);
+      console.log("B");
+      const lines = csv.split("\n");
+      result = [];
+      const headers = lines[0].split(",");
+      let counter = 0;
+      for (const line of lines) {
+        counter++;
+        const obj = {};
+        const currentLine = line.split(",");
+        for (const [index, header] of headers.entries()) {
+          obj[header] = currentLine[index];
         }
-        console.log("counter:", counter);
-        res.status(200).json(result);
-        
-    });
+        result.push(obj);
+      }
+      console.log("counter:", counter);
+      res.status(200).json(result);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("An error occurred");
+    }
   }
