104d5783211dc4121daed0f191663d655a338fa3
Marlon Falcón Hernández <falconsoft.3d@gmail.com>
2022-12-31 15:36:17 +0100

    sc

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ export default function App({ Component, pageProps }) {
   return (
      <>
          <Script src="vendor/jquery/jquery.min.js" />
-        <Script src="vendor/jquery-easing/jquery.easing.min.js" />
+        {/* <Script src="vendor/jquery-easing/jquery.easing.min.js" /> */}
         <Script src="vendor/bootstrap/js/bootstrap.bundle.min.js" />
         <Script src="js/sb-admin-2.min.js" />
         {/* <Script src="js/demo.js" /> */}
